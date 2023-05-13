dd01bcd5b4eaf5481252289b294dad4a1a73ab19
Mark D. Lalich <mdlalich@gmail.com>
2023-05-12 22:53:47 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log('Dr. Big D!')
---
+++ solutions.js
@@ -0,0 +1,135 @@
+{
+// create array of team members
+    const person = [
+    {name: 'bob', employed: true, vacationing: true}, // "ENJOY YOUR NAP!"
+    {name: 'jay', employed: true, vacationing: true},// "ENJOY YOUR NAP!"
+    {name: 'silentService', employed: true, vacationing: false}, // BEEP BOOP BOP!!!
+    {name: 'snakeOilS', employed: false, vacationing: true}, // "ENJOY YOUR NAP!"
+]
+
+// create function set alarm, if person has job and is not vacationing
+
+const setAlarm = () => {
+   if (employed = true, vacationing = false)
+        return(console.log(person.name + 'beep boop bop!!!' ))
+    else (console.log(person.name + 'Enjoy your Nap!'))
+
+}
+    // setAlarm(true, true) // => returns false
+    // setAlarm(false, false) // => returns false
+    // setAlarm(false, true) // => returns false
+    // setAlarm(true, false) // => returns = true
+
+    console.log(person)
+
+person.forEach(name => {
+    return(setAlarm(person[0].name))
+})
+}
+// // make a odd number counter via function ------------ODD NUMBER COUNTER-----------
+
+
+ {
+
+// function OddNumberCount() {
+// let i = 0; i <100; i --;
+//     if (i % 2 == 1){
+//          return console.log(i + 1);
+//      }else { return 'even';
+//  }
+//  OddNumberCount(7)
+//  console.log(i)
+//  }
+ }
+//oddNumberCount(7) // => returns 3 (because [1, 3, 5] are the positive odd numbers below 7)
+//oddNumberCount(16) // => returns 8 (because [1, 3, 5, 7, 9, 11, 13, 15] are the positive odd numbers below 7)
+
+// -----------------DISEMVOWLING TROLLS ------------------------ //
+
+// {   
+// // create function to remove doodoo "trollBeGone"
+//     trollBeGone = () => {     /// trollsBeGone("This website is for losers LOL") // => returns "Ths wbst s fr lsrs LL"
+//         const vowels = 'aieouAEIOU'; // define doodoo vowels
+
+//         for (let i = 0; i < ("").length; i =+1)  //loops through function string
+//             return mystring.split; // splits letters up to be removed
+//             console.log(mystring.split(""ß)) 
+
+//    for (let j = 0; j < length.vowels; j =+1)  // loops vowels
+
+// (mystring.split).push(j) // push out vowels in current string
+// return stringN;ß
+//   console.log(stringN) }
+
+
+
+// trollsBeGone("This website is for losers LOL") // => returns "Ths wbst s fr lsrs LL"
+
+// }
+// { // I ASKED CHAT GPT HERE WHY I WAS HAVING SUCH PROBLEMS AND THIS IS WHAT IT DIRECTED ME TO USE, STILL WAS NOT ABLE TO GET IT TO WORK!!!
+// trollBeGone = () => {
+//     let str = () => {
+//     return str.replace(/[aieouAEIOU]/gi, '')
+// }
+// trollsBeGone("This website is for losers LOL")
+// }
+// console.log(trollBeGone)
+// }
+
+// BANK ACCOUNT SUMMARY //
+
+// {
+// const bankInfo = [              // creatae bankInfo array
+//     {name: 'savings', balance: 600},
+//     {name: 'checking', balance: 800},
+//     {name: 'moneyMarket', balance: 200},
+//     {name: 'creditCard', balance: -2000}
+// ]
+
+// bankAccountSummary = () => {        // function for bank account summary
+//     for (let balance of bankInfo) {        // pull info
+//         const bankTotal // create new variable to reference from funciton
+//              = sum [bankInfo.balance] // calculate the value
+        
+    
+//     bankAccountSummary(bankInfo.balance)
+//     console.log(bankTotal) // log the value
+
+// inTheRed = () => {          // define inTheRed function
+//     if (bankTotal <= 0) {
+//     return true;            // return T/F
+
+// }}
+// console.log(inTheRed())
+
+
+// }}}
+
+// RD II with help still not seeing it happen though as supposed to this week will be a very busy study week...
+// {
+// const bankInfo = {
+//     savings: 600,
+//     chekcing: 800,
+//     moneyMarket: 200,
+//     creditCard: -2000
+// };
+// function bankAccountSummary(bankInfo) {
+//     let total = 0;
+//     for (let key in bankInfo) {
+//         total += bankInfo[key];
+//     }
+//     return total;
+//     console.log(bankInfo)
+    
+// }
+// const bankTotal = bankAccountSummary(bankInfo);
+
+// function inTheRed(total) {
+//     return total <0;
+// }  console.log(inTheRed())
+// }
+
+
+//---------------// MAD LIBS //------------------/
+
+
